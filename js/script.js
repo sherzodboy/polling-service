4ce5595c2d2389457158705ae3298bd9bcfc13a5
sherzod02 <kholmamatovsh09>
2022-07-19 17:50:06 +0500

    added js functions and styled

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ let poll = {
   question: "What is your favourite programming language?",
   answers: ["Html", "Css", "Jacascript", "React,js"],
   pollCount: 20,
-  answersWeight: [4, 4, 2, 10],
-  selectAnswers: -1,
+  answersWeight: [7, 7, 6, 5],
+  selectedAnswer: -1,
 };
 
 let pollDom = {
@@ -14,3 +14,43 @@ let pollDom = {
 };
 
 pollDom.question.innerHTML = poll.question;
+pollDom.answers.innerHTML = poll.answers
+  .map(function (answer, i) {
+    return `
+      <div class="answer" onclick="markAnswer(${i})">
+        ${answer}
+        <span class="percentage-bar"></span>
+        <span class="percentage-value"></span>
+      </div>
+    `;
+  })
+  .join("");
+
+function markAnswer(i) {
+  poll.selectedAnswer = +i;
+  try {
+    document
+      .querySelector(".poll .answers .answer .selected")
+      .classList.remove("selected");
+  } catch (msg) {
+    document
+      .querySelectorAll(".poll .answers .answer")
+      [+i].classList.add("selected");
+  }
+
+  showResults();
+}
+
+function showResults() {
+  let answers = document.querySelectorAll(".poll .answers .answer");
+  for (let i = 0; i < answers.length; i++) {
+    let percentage = 0;
+    if (i === poll.selectedAnswer) {
+      percentage = Math.round(
+        (poll.answersWeight[i] * 100) / (poll.pollCount + 1)
+      );
+    }
+    answers[i].querySelector(".percentage-bar").style.width = percentage + "%";
+    answers[i].querySelector(".percentage-value").innerHTML = percentage + "%";
+  }
+}
