f98e826ebae2b434bd3fc66dc8ac0d7c0517f712
sherzod02 <kholmamatovsh09>
2022-07-21 22:03:55 +0500

    added new poll

--- js/script.js
+++ js/script.js
@@ -1,63 +1,26 @@
 "use strict";
 
-let poll = {
-  question: "What is your favourite programming language?",
-  answers: ["Html", "Css", "Bootstrap", "Javascript", "React.js"],
-  pollCount: 20,
-  answersWeight: [19, 20, 18, 14, 16],
-  selectedAnswer: -1,
-};
+// Test your knowladge
+const options = document.querySelectorAll(".options-label");
 
-let pollDom = {
-  question: document.querySelector(".poll .question"),
-  answers: document.querySelector(".poll .answers"),
-};
-
-pollDom.question.innerHTML = poll.question;
-pollDom.answers.innerHTML = poll.answers
-  .map(function (answer, i) {
-    return `
-      <div class="answer" onclick="markAnswer(${i})">
-        ${answer}
-        <span class="percentage-bar"></span>
-        <span class="percentage-value"></span>
-      </div>
-    `;
-  })
-  .join("");
-
-function markAnswer(i) {
-  poll.selectedAnswer = +i;
-  try {
-    document
-      .querySelector(".poll .answers .answer .selected")
-      .classList.remove("selected");
-  } catch (msg) {
-    document
-      .querySelectorAll(".poll .answers .answer")
-      [+i].classList.add("selected");
-  }
-
-  showResults();
-}
-
-function showResults() {
-  let answers = document.querySelectorAll(".poll .answers .answer");
-  for (let i = 0; i < answers.length; i++) {
-    let percentage = 0;
-    if (i === poll.selectedAnswer) {
-      percentage = Math.round(
-        (poll.answersWeight[i] * 100) / (poll.pollCount + 1)
-      );
+for (let i = 0; i < options.length; i++) {
+  options[i].addEventListener("click", () => {
+    for (let k = 0; k < options.length; k++) {
+      if (options[k].classList.contains("selected")) {
+        options[k].classList.remove("selected");
+      }
     }
-    answers[i].querySelector(".percentage-bar").style.width = percentage + "%";
-    answers[i].querySelector(".percentage-value").innerHTML = percentage + "%";
-  }
+
+    options[i].classList.add("selected");
+
+    for (let j = 0; j < options.length; j++) {
+      options[j].classList.add("selectall");
+    }
+  });
 }
 
 // Checkboxes
-
-var checkboxes = document.querySelectorAll("input[type = 'checkbox']");
+const checkboxes = document.querySelectorAll("input[type = 'checkbox']");
 
 function checkAll(myCheckbox) {
   if (myCheckbox.checked == true) {
