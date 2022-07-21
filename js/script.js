f092a6e14801f0eea8eba2f4d5a1b84348f4f1fc
sherzod02 <kholmamatovsh09>
2022-07-21 18:07:23 +0500

    added checkboxes

--- js/script.js
+++ js/script.js
@@ -54,3 +54,19 @@ function showResults() {
     answers[i].querySelector(".percentage-value").innerHTML = percentage + "%";
   }
 }
+
+// Checkboxes
+
+var checkboxes = document.querySelectorAll("input[type = 'checkbox']");
+
+function checkAll(myCheckbox) {
+  if (myCheckbox.checked == true) {
+    checkboxes.forEach(function (checkbox) {
+      checkbox.checked = true;
+    });
+  } else {
+    checkboxes.forEach(function (checkbox) {
+      checkbox.checked = false;
+    });
+  }
+}
